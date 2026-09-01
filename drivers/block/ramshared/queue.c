ef1301df4426b541416f2d969109b93ce17041b7
emersonbusson <93008583+emersonbusson@users.noreply.github.com>
2026-09-01 02:26:48 +0000

    refactor(kernel): enforce semantic blk_status_t bounds checks\n\nImplemented precise semantic blk_status_t and kernel errno returns for unaligned and out-of-bounds requests in ramshared device queue. Improved bounds checking using check_mul_overflow and offset vs capacity validations to prevent kernel panic vectors.

--- drivers/block/ramshared/queue.c
+++ drivers/block/ramshared/queue.c
@@ -14,6 +14,10 @@
 #include "ramshared.h"
 #include "compat.h"
 
+#ifndef check_mul_overflow
+#define check_mul_overflow(a, b, d) __builtin_mul_overflow(a, b, d)
+#endif
+
 static blk_status_t ramshared_process_bio(struct ramshared_device *rs_dev,
 					  struct bio *bio, loff_t pos)
 {
@@ -49,7 +53,7 @@ static blk_status_t ramshared_queue_rq(struct blk_mq_hw_ctx *hctx,
 {
 	struct request *rq = bd->rq;
 	struct ramshared_device *rs_dev = rq->q->queuedata;
-	loff_t pos = (loff_t)blk_rq_pos(rq) << RAMSHARED_SECTOR_SHIFT;
+	loff_t pos;
 	size_t len = blk_rq_bytes(rq);
 	blk_status_t status = BLK_STS_OK;
 	struct bio *bio;
@@ -57,11 +61,22 @@ static blk_status_t ramshared_queue_rq(struct blk_mq_hw_ctx *hctx,
 	if (unlikely(!rs_dev || !rs_dev->dma.cpu_addr))
 		return BLK_STS_IOERR;
 
-	if (unlikely(pos + len > rs_dev->capacity_bytes)) {
+	if (unlikely(check_mul_overflow((loff_t)blk_rq_pos(rq), (loff_t)RAMSHARED_SECTOR_SIZE, &pos)))
+		return BLK_STS_IOERR;
+
+	if (unlikely(pos < 0 || len > rs_dev->capacity_bytes || pos > rs_dev->capacity_bytes - len)) {
 		dev_err_ratelimited(rs_dev->dev,
 			"I/O bounds violation: pos=%lld, len=%zu, cap=%llu\n",
 			pos, len, rs_dev->capacity_bytes);
 		return BLK_STS_IOERR;
+	}
+
+	if (req_op(rq) != REQ_OP_FLUSH) {
+		if (unlikely(pos % 4096 != 0 || len % 4096 != 0)) {
+			blk_mq_start_request(rq);
+			blk_mq_end_request(rq, BLK_STS_NOTSUPP);
+			return BLK_STS_OK;
+		}
 	}
 
 	blk_mq_start_request(rq);
@@ -104,17 +119,23 @@ static int ramshared_bdev_rw_page(struct block_device *bdev, sector_t sector,
 				  struct page *page, enum req_op op)
 {
 	struct ramshared_device *rs_dev = bdev->bd_disk->private_data;
-	loff_t pos = (loff_t)sector << RAMSHARED_SECTOR_SHIFT;
+	loff_t pos;
 	size_t len = PAGE_SIZE;
 	void __iomem *vram_ptr;
 	void *mem;
 	int is_write = op_is_write(op);
 
 	if (unlikely(!rs_dev || !rs_dev->dma.cpu_addr))
-		return -EIO;
+		return -ENODEV;
 
-	if (unlikely(pos + len > rs_dev->capacity_bytes))
-		return -EIO;
+	if (unlikely(check_mul_overflow((loff_t)sector, (loff_t)RAMSHARED_SECTOR_SIZE, &pos)))
+		return -ERANGE;
+
+	if (unlikely(pos < 0 || len > rs_dev->capacity_bytes || pos > rs_dev->capacity_bytes - len))
+		return -ERANGE;
+
+	if (unlikely(pos % 4096 != 0 || len % 4096 != 0))
+		return -EOPNOTSUPP;
 
 	vram_ptr = rs_dev->dma.cpu_addr + pos;
 	mem = kmap_local_page(page);
