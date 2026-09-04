667c15fd4825071e0d23aadea7345b34c2703161
emersonbusson <93008583+emersonbusson@users.noreply.github.com>
2026-09-04 16:44:54 +0000

    refactor(dma): guard clauses and explicit nullification in ramshared_dma_cleanup

--- drivers/block/ramshared/dma.c
+++ drivers/block/ramshared/dma.c
@@ -51,6 +51,8 @@ void ramshared_dma_cleanup(struct ramshared_device *rs_dev)
 	if (!rs_dev)
 		return;
 
-	rs_dev->dma.cpu_addr = NULL;
-	rs_dev->dma.size = 0;
+	if (rs_dev->dma.cpu_addr) {
+		rs_dev->dma.cpu_addr = NULL;
+		rs_dev->dma.size = 0;
+	}
 }
